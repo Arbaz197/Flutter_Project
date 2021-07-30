fa2a15ac14b59c45726f24ef36a233f12bfbf580
SalmanAli6944 <ahmerali408@gmail.com>
2021-07-30 17:34:53 +0500

    changes

--- lib/BottomTabPages/property.dart
+++ lib/BottomTabPages/property.dart
@@ -42,102 +42,101 @@ class _PropertyState extends State<Property> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                  hintText: 'Search Property...',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FilterPage()));
-                    },
-                    icon: Icon(
-                      Icons.filter_list_sharp,
-                      color: Color(0xfffcb812),
-                      size: 35,
-                    ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                hintText: 'Search Property...',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => FilterPage()));
+                  },
+                  icon: Icon(
+                    Icons.filter_list_sharp,
+                    color: Color(0xfffcb812),
+                    size: 35,
                   ),
                 ),
               ),
             ),
-            SizedBox(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // ignore: deprecated_member_use
-                RaisedButton.icon(
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  label: Text(
-                    'BUY',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  icon: Icon(
-                    Icons.home_sharp,
-                    color: Colors.white,
-                  ),
-                  textColor: Colors.white,
-                  color: Color(0xfffcb812),
+          ),
+          SizedBox(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // ignore: deprecated_member_use
+              RaisedButton.icon(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                label: Text(
+                  'BUY',
+                  style: TextStyle(color: Colors.white),
                 ),
-                // ignore: deprecated_member_use
-                RaisedButton.icon(
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  label: Text(
-                    'RENT',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  icon: Icon(
-                    Icons.vpn_key_rounded,
-                    color: Colors.white,
-                  ),
-                  textColor: Colors.white,
-                  color: Color(0xfffcb812),
+                icon: Icon(
+                  Icons.home_sharp,
+                  color: Colors.white,
                 ),
-                // ignore: deprecated_member_use
-                RaisedButton.icon(
-                  onPressed: () {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  label: Text(
-                    'INVEST',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  icon: Icon(
-                    Icons.attach_money_sharp,
-                    color: Colors.white,
-                  ),
-                  //textColor: Colors.white,
-                  color: Color(0xfffcb812),
+                textColor: Colors.white,
+                color: Color(0xfffcb812),
+              ),
+              // ignore: deprecated_member_use
+              RaisedButton.icon(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                label: Text(
+                  'RENT',
+                  style: TextStyle(color: Colors.white),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 18,
-            ),
-            Row(
+                icon: Icon(
+                  Icons.vpn_key_rounded,
+                  color: Colors.white,
+                ),
+                textColor: Colors.white,
+                color: Color(0xfffcb812),
+              ),
+              // ignore: deprecated_member_use
+              RaisedButton.icon(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                label: Text(
+                  'INVEST',
+                  style: TextStyle(color: Colors.white),
+                ),
+                icon: Icon(
+                  Icons.attach_money_sharp,
+                  color: Colors.white,
+                ),
+                //textColor: Colors.white,
+                color: Color(0xfffcb812),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "            Popular",
+                  "Popular",
                   style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
                 Text(
-                  "    Featured",
+                  "Featured",
                   style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
                 Text(
@@ -146,178 +145,105 @@ class _PropertyState extends State<Property> {
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Divider(
-                height: 10,
-                color: Colors.black45,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Divider(
+              height: 10,
+              color: Colors.black45,
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Property_Card(),
+                  Property_Card(),
+                  Property_Card(),
+                  Property_Card(),
+                  Property_Card(),
+                  Property_Card(),
+                  Property_Card(),
+                  Property_Card(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
             ),
-            Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      height: 300,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            ),
-                          ]),
-                    ),
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-                      height: 200,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey[700],
-                          image: DecorationImage(
-                              image: AssetImage(
-                                  'assets/images/abaadee logo white.png'))),
-                    ),
-                    Positioned(
-                      left: 350,
-                      top: 25,
-                      child: CircleAvatar(
-                        backgroundColor: Color(0xfffcb812),
-                        child: Icon(
-                          Icons.favorite_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                        top: 230,
-                        left: 50,
-                        child: Column(
-                          children: [
-                            Positioned(
-                              child: Text(
-                                "Hello salman gandu",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            Text(
-                              "Hello  gandu",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 18),
-                            )
-                          ],
-                        ))
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Stack(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      height: 300,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            ),
-                          ]),
-                    ),
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey[700],
-                        image: DecorationImage(
-                            image: AssetImage(
-                                'assets/images/abaadee logo white.png')),
-                      ),
-                    ),
-                    Positioned(
-                      left: 350,
-                      top: 25,
-                      child: CircleAvatar(
-                        backgroundColor: Color(0xfffcb812),
-                        child: Icon(
-                          Icons.favorite_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Stack(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      height: 300,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            ),
-                          ]),
-                    ),
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-                      height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey[700],
-                        image: DecorationImage(
-                            image: AssetImage(
-                                'assets/images/abaadee logo white.png')),
-                      ),
-                    ),
-                    Positioned(
-                      left: 350,
-                      top: 25,
-                      child: CircleAvatar(
-                        backgroundColor: Color(0xfffcb812),
-                        child: Icon(
-                          Icons.favorite_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-              ],
-            )
-          ],
-        ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class Property_Card extends StatelessWidget {
+  const Property_Card({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          height: 300,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ]),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+          height: 200,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey[700],
+              image: DecorationImage(
+                  image: AssetImage('assets/images/abaadee logo white.png'))),
+        ),
+        Positioned(
+          left: 350,
+          top: 30,
+          child: CircleAvatar(
+            backgroundColor: Color(0xfffcb812),
+            child: Icon(
+              Icons.favorite_rounded,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+            top: 230,
+            left: 50,
+            child: Column(
+              children: [
+                Positioned(
+                  child: Text(
+                    "Hello salman gandu",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  "Hello  gandu",
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                )
+              ],
+            ))
+      ],
     );
   }
 }
